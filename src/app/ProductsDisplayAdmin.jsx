228fd19fdedcdf13772a5548d52bbbefa59000a5
Kinnari-Shah <iampritesh13@gmail.com>
2023-05-09 12:47:51 +0530

    Updae Comments and add files to understand the code....

--- src/app/ProductsDisplayAdmin.jsx
+++ src/app/ProductsDisplayAdmin.jsx
@@ -4,8 +4,10 @@ import React, { useState } from 'react'
 import axios from 'axios';
 
 const ProductsDisplayAdmin = () => {
-
-    const [myRenderId, setMyRenderId] = useState("")
+    // ----------------------------------------------------
+    // we will use "useState" to save that RenderId....
+    const [myRenderId, setMyRenderId] = useState("");
+    // ----------------------------------------------------
 
     const RenderId = async () => {
 
@@ -47,41 +49,127 @@ const ProductsDisplayAdmin = () => {
                         "priceTotal": 9000
                     }
                 ],
-                "total": 30000
+                "total": 26000
             }
         }
 
-        await axios.post('https://render.carbone.io/render/:1cc8186b6b3860a372993db880d6b1f01a981ee12ea6fb7308f5715c9ca1705c', data, {
-            headers: {
-                'Content-Type': 'application/json',
+        // Here paste your :: (Account data :: 03) Template ID ::
+        // Url format is  :: 
+        await axios.post('https://render.carbone.io/render/:1cc8186b6b3860a372993db880d6b1f01a981ee12ea6fb7308f5715c9ca1705c',
 
-                'Authorization': 'test_eyJhbGciOiJFUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI2MjE0OTU2OTUyMzQwMTUzMDgiLCJhdWQiOiJjYXJib25lIiwiZXhwIjoyMzQ1ODAzMjYxLCJkYXRhIjp7ImlkQWNjb3VudCI6IjYyMTQ5NTY5NTIzNDAxNTMwOCJ9fQ.ALjXfqpXOQoob7cz2CCk32pYAQ7HgFvxXdTy7FsfcK_BEtj_yZZy1S8ztJgr-I0punm-2C4aKziPm_DiI_EVuWrbAWDsIywmSFmo_EyRF8Riis9iDqloyBOpbdnr56-qa0sQ2NXnd-_cRMkPjhum8qZHnd9_0BI3WsJJ4o_BXSmMfUiN',
+            //  Goto the 
+            // "Quickstart Carbone Cloud API" Section of 
+            // "documentation" page of 
+            // "Carbone-Js" Official Websie....
 
-                'carbone-version': '4'
-            }
-        }).then((response) => {
-            console.log(response.data.data.renderId);
-            setMyRenderId(response.data.data.renderId);
-        }, (error) => {
-            console.log(error);
-        });
+            // => Click on this Section Link::
+            // https://carbone.io/documentation.html#quickstart-carbone-cloud-api
+
+            // --------------------()--------- ()--------- ()-----------------
+
+            // => Then Goto 3rd step of that section.....
+            // -----------------------------------------------
+            //     "3 - Generate a report from the template ID"
+            // --------------------------------------------
+
+            //     And copy "purply Highlited" Format of the url::
+
+            //     -----------------------------------------------------------
+            // :: POST https://render.carbone.io/render/:templateID ::
+            // -----------------------------------------------------------
+
+            // To Get templateID ::
+            // ------------------------
 
 
-        await axios.get(`https://api.carbone.io/render/${myRenderId}`).then((response) => {
-            console.log(response.data);
-        }, (error) => {
-            console.log(error);
-        });
+            //  ------------------------------
+            // https://studio.carbone.io/
+            // ------------------------------
+
+            // Then Click on :: 
+            // --------------
+            // Second Navbar => in Right side => Two icons => 
+            // 1) Info and 2) settings => Click on Info Icon......  
+
+            // Then Copy your 2nd and 3rd important data....
+            // ---------------------------------------
+            // 2) Filename :: 
+            // smt5
+
+            // 3) Template ID   ::
+            // 1cc8186b6b3860a372993db880d6b1f01a981ee12ea6fb7308f5715c9ca1705c
+
+            // -------------------------------------------------------
+
+            data,
+
+            // :: let's Do the "02 thing" :: Data ::
+
+            // So, We are on the Page  :: 
+            // https://carbone.io/documentation.html#quickstart-carbone-cloud-api
+
+            // => Then Goto 3rd step of that section.....
+            // -----------------------------------------------
+            // Copy this 
+            // "intial code" of "data" of our  "axios.post" Request" from section.....
+
+            // ------------------------------------
+            // {
+            //   "convertTo": "pdf",
+            //   "data": {
+            //     "firstname": "John",
+            //     "lastname": "Wick"
+            //   }
+            // }
+            // ------------------------------------
+
+            // :: this Object's 2nd attribute :: "data" 's value::
+            // :: We will change this "data" atribute's value....
+
+            //     Goto :: https://studio.carbone.io/ ::
+            // :: and copy:: "the Left section" :: of:: main body::
+
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    // Here paste your :: (Account data :: 01) Test API key ::
+                    'Authorization':
+                        'test_eyJhbGciOiJFUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI2MjE0OTU2OTUyMzQwMTUzMDgiLCJhdWQiOiJjYXJib25lIiwiZXhwIjoyMzQ1ODAzMjYxLCJkYXRhIjp7ImlkQWNjb3VudCI6IjYyMTQ5NTY5NTIzNDAxNTMwOCJ9fQ.ALjXfqpXOQoob7cz2CCk32pYAQ7HgFvxXdTy7FsfcK_BEtj_yZZy1S8ztJgr-I0punm-2C4aKziPm_DiI_EVuWrbAWDsIywmSFmo_EyRF8Riis9iDqloyBOpbdnr56-qa0sQ2NXnd-_cRMkPjhum8qZHnd9_0BI3WsJJ4o_BXSmMfUiN',
+
+                    // https://account.carbone.io/ 
+                    // => My Subscription Box 
+                    // => Test API key
+
+                    // => Email id:: iampritesh13 @gmail.com
+                    // => Passward:: Kashi @281055
+
+                    'carbone-version': '4'
+                }
+            }).then((response) => {
+                // ----------------------------------------------------
+                // So, "Carbone Cloud API" will give "renderId" in the response of "POST" Request.....
+                console.log(response.data.data.renderId);
+                // ----------------------------------------------------
+                // we will use "useState" to save that RenderId....
+                setMyRenderId(response.data.data.renderId);
+                // ----------------------------------------------------
+            }, (error) => {
+                console.log(error);
+            });
 
     };
 
     return (
         <>
-
+            {/* Now , Create a Two Button........... */}
+            {/* To send the data to API via "POST" Request.....    */}
+            {/* and Get the  RenderID of the PDF from API  */}
             <button onClick={() => {
                 RenderId();
             }}>RenderId  </button>
 
+
+            {/*  To Download the pdf Created at the "RenderID" Url..... */}
             {
                 (myRenderId) &&
                 <>
